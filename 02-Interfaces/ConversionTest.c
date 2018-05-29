42ac6d58de3794719ea50237ae60e67c117d3bc9
misaelmagi <misastmagi@gmail.com>
2018-05-29 14:52:37 -0300

    Agregados asserts

--- 02-Interfaces/ConversionTest.c
+++ 02-Interfaces/ConversionTest.c
@@ -1,5 +1,11 @@
 #include <assert.h>
 #include "Conversion.h"
 int main(){
-	assert(Celsius(18.5)==-7.5);
+	assert(Celsius(18.5)==-7.5)
+	assert(Celsius(0.0)==-17.77)
+	assert(Celsius(32.0)==0);
+	assert(Farenheit(0.0)==32.0);
+	assert(Farenheit(100.0)==212.0);
+	assert(Farenheit(-17.77)==0);
+	
 }
